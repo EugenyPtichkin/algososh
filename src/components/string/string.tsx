c6c3b66a91af3b22d3c09e9918ef6530648c5578
Eugeny Ptichkin <eugeny.ptichkin@novsu.ru>
2024-03-03 01:49:56 +0300

    перестановка строки без подсветки

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -25,75 +25,62 @@ export const StringComponent: React.FC = () => {
     invertString(inputString);
     setCurrentValue(0);
     reverseStr(inputString);
-    setIsLoader(false);
   }
 
-  //создать пустой двумерный массив для хранения всех значений строк на всех этапах перестановок
-  //const n: number = inputString.length;
-  //const m: number = Math.floor(n / 2) + 1;
-  //let swapArr = Array.apply(null, Array(m)).map(function () {
-  //   return Array.apply(null, Array(n));
-  //});
-  let swapString : string[] = [];
+  let swapString: string[] = [];
 
-  const invertString = (inString: string) => {    
+
+  const invertString = (inString: string) => {
     swapString = [];
     let symbols: string[] = new Array;
     symbols = Array.from(inString);  //преобразовать строку в символы
-    //console.log(`first symbols=${symbols}`);
 
-
-    //заполнить выходной массив входной строкой (в нулевую позицию)
-    //for (let j: number = 0; j < inString.length; j++) {
-    //  swapArr[0][j] = symbols[j];      
-    //}
     swapString.push(symbols.join(''));
-    console.log(`swapString[0]=${swapString}`);
+    //console.log(`swapString=${swapString}`);
 
     //заполнить выходной массив отображаемыми строками на каждом этапе перестановок
     let swap: string = '';
     for (let i: number = 0; i < Math.floor(inString.length / 2); i++) {
-      //перестановка двух крайних элементов
+      //перестановка по два крайних элемента
       swap = symbols[i];
       symbols[i] = symbols[inString.length - 1 - i];
       symbols[inString.length - 1 - i] = swap;
 
-      //запись в массив отображения строки в текущем состоянии (с 1 позиции)
-      //for (let j: number = 0; j < inString.length; j++) {
-      //  swapArr[i + 1][j] = symbols[j];
-      //}
-      swapString.push(symbols.join(''));                    
-      console.log(`swapString=${swapString}`);
+      swapString.push(symbols.join(''));
+      //console.log(`swapString=${swapString}`);
     }
   }
   
-  const sleep = () => new Promise ((resolve) => setTimeout(resolve, DELAY_IN_MS));
+  //функция-ожидание
+  const sleep = () => new Promise((resolve) => setTimeout(resolve, DELAY_IN_MS));
 
+  //функция прокрутки во времени массива строк
   const reverseStr = async (inString: string) => {
-  let maxSwapValue = Math.floor(inString.length / 2) + 1;
-    let currentString : string = '';
-    let tmp : number = 0;
+    let maxSwapValue = Math.floor(inString.length / 2) + 1;
+    let currentString: string = '';
+    let tmp: number = 0;
     while (tmp < maxSwapValue) {
-      currentString = swapString.shift() || '';  
-      setShowString(currentString);            
+      currentString = swapString.shift() || '';
+      setShowString(currentString);
       await sleep();
       tmp++;
+      //console.log(`nextValue=${tmp} maxValue=${maxSwapValue}`);
       setCurrentValue(tmp);
-      }    
+    }
+    setIsLoader(false);
   }
 
   return (
     <SolutionLayout title="Строка" >
       <form className={styles.content} onSubmit={onSubmit}  >
         <Input type="text" maxLength={11} isLimitText={true} onChange={onChange} value={inputString} />
-        <Button text="Развернуть" disabled={!inputString} isLoader={isLoader} type = 'submit'/>
+        <Button text="Развернуть" disabled={!inputString} isLoader={isLoader} type='submit' />
       </form>
-      <ul>
-        {/*showString.split('').map((symbol,index) => (
-        <li key={index}> 
-         <Circle letter={symbol} />
-        </li> )) */}
-         {showString}
+      <ul className={styles.outString}>
+        {showString.split('').map((symbol, index) => (
+          <li key={index}>
+            <Circle letter={symbol} />
+          </li>))}
       </ul>
     </SolutionLayout>
   );
